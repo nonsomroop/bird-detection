eca707063cf8dbeb44c229de8a3e04f30f46b922
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-10-13 22:17:00 +0700

    [add] 12/10/2024

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def play_sound():
     return app.send_static_file('sound/test_sound.aac')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
